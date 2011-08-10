e0089965a1272d0be772f24e224191c906b1b269
inner-whisper <maks.rydkin@gmail.com>
2011-08-10 14:24:17 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '8f252b6be81623e0162b7009454a52df2395723eaf12337fec5dec988ab363270b31bf2509e4a4558878af2c70d6756c3b4561a644fd2f57bc773fb08c23ea56'
